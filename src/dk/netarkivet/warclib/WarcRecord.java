a1e8c0a89228a90c5eb1820dec8155af3f0bcc7f
Nicholas Clarke <nicl@kb.dk>
2011-10-25 18:29:28 +0200

    Added utf8 support to header linereader. Seems to works. Tests not conclusive. Needs tweaking and more unit tests.

--- src/dk/netarkivet/warclib/WarcRecord.java
+++ src/dk/netarkivet/warclib/WarcRecord.java
@@ -802,8 +802,12 @@ public class WarcRecord {
     	StringBuilder sb = new StringBuilder(128);
     	int state = S_START;
     	int c;
+    	int utf8_c;
+    	byte utf8_read;
+    	byte utf8_octets;
     	boolean bCr = false;
     	boolean bLoop = true;
+    	boolean bOctets;
     	while (bLoop) {
     		c = in.read();
     		if (c == -1) {
@@ -916,7 +920,83 @@ public class WarcRecord {
     					// Misplaced CR.
         				bCr = false;
     				}
-    				sb.append((char)c);
+    				if ((c & 0x80) == 0x00) {
+    					// US-ASCII: 0000 0000-0000 007F | 0xxxxxxx
+        				sb.append((char)c);
+    				} else {
+    					// UTF-8
+    					utf8_read = 1;
+    					bOctets = true;
+    					if ((c & 0xE0) == 0xC0) {
+        					// UTF-8: 0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+        					utf8_c = c & 0x1F;
+        					utf8_octets = 2;
+        				} else if ((c & 0xF0) == 0xE0) {
+        					// UTF-8: 0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+        					utf8_c = c & 0x0F;
+        					utf8_octets = 3;
+        				} else if ((c & 0xF8) == 0xF0) {
+        					// UTF-8: 0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+        					utf8_c = c & 0x07;
+        					utf8_octets = 4;
+        				} else {
+        					// Invalid UTF-8 octet.
+        					utf8_c = 0;
+        					utf8_read = 0;
+        					utf8_octets = 0;
+        					bOctets = false;
+        				}
+    					// Read the remaning octets.
+    					while (bOctets) {
+    						if (utf8_read < utf8_octets) {
+        						c = in.read();
+        						if (c == -1) {
+        			    			// EOF.
+        							bOctets = false;
+        							bLoop = false;
+        						} else if ((c & 0xC0) == 0x80) {
+        							utf8_c = (utf8_c << 6) | (c & 0x3F);
+            						++utf8_read;
+        						} else {
+                					// Invalid UTF-8 octet.
+        							bOctets = false;
+        						}
+    						}
+    						else {
+    							bOctets = false;
+    						}
+    					}
+    					// Correctly encoded.
+    					if (utf8_read == utf8_octets) {
+        					switch (utf8_octets) {
+        					case 2:
+            					if (utf8_c < 0x00000080 || utf8_c > 0x000007FF) {
+            						// Incorrectly encoded value.
+            					}
+        						break;
+        					case 3:
+        						if (utf8_c < 0x00000800 || utf8_c > 0x0000FFFF) {
+            						// Incorrectly encoded value.
+        						}
+        						break;
+        					case 4:
+        						if (utf8_c < 0x00010000 || utf8_c > 0x0010FFFF) {
+            						// Incorrectly encoded value.
+        						}
+        						break;
+        					}
+        					sb.append((char) utf8_c);
+    					}
+    				}
+    				/*
+    				Char. number range  |        UTF-8 octet sequence
+    				      (hexadecimal)    |              (binary)
+    				   --------------------+---------------------------------------------
+    				   0000 0000-0000 007F | 0xxxxxxx
+    				   0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+    				   0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+    				   0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+    				*/
    					break;
     			}
     			break;
@@ -935,5 +1015,4 @@ public class WarcRecord {
     	}
     	return warcHeader;
     }
-
 }
