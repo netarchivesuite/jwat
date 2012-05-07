a7bd2d23b5fab84163c992c90f6b6d47ff30ae6b
Nicholas Clarke <nicl@kb.dk>
2012-05-07 19:12:25 +0200

    Javadocs for HeaderLineReader.

--- jwat-common/src/main/java/org/jwat/common/HeaderLineReader.java
+++ jwat-common/src/main/java/org/jwat/common/HeaderLineReader.java
@@ -21,19 +21,39 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
 
+/**
+ * Advanced header/line reader which can be configured into difference modes.
+ * The reader can either read normal lines or header lines.
+ * Supported encodings are raw, US-ASCII, ISO-8859-1 and UTF-8.
+ *
+ * Furthermore header line can employ LWS, quoted text and encoded words.
+ *
+ * After calling the readLine method additional information is available from
+ * public fields on the reader.
+ *
+ * @author nicl
+ */
 public class HeaderLineReader {
 
     /*
      * Internal states.
      */
 
+	/** Initial state for reading a normal line. */
     protected static final int S_LINE = 0;
+    /** Initial state for reading a header line. */
     protected static final int S_NAME = 1;
+    /** State for reading a header value. */
     protected static final int S_VALUE = 2;
+    /** State for reading a LWS character sequence. */
     protected static final int S_LWS = 3;
+    /** State for reading a quoted string. */
     protected static final int S_QUOTED_TEXT = 4;
+    /** State for reading a quoted pair character. */
     protected static final int S_QUOTED_PAIR = 5;
+    /** State for reading a quoted LWS character sequence. */
     protected static final int S_QUOTED_LWS = 6;
+    /** Status for reading an encoded word character sequence. */
     protected static final int S_ENCODED_WORD_EQ = 7;
 
     /*
@@ -70,35 +90,52 @@ public class HeaderLineReader {
      * Encoding.
      */
 
+    /** Raw encoding identifier. */
     public static final int ENC_RAW = 0;
+    /** US-ASCII encoding identifier. */
     public static final int ENC_US_ASCII = 1;
+    /** ISO-8859-1 encoding identifier. */
     public static final int ENC_ISO8859_1 = 2;
+    /** UTF-8 encoding identifier. */
     public static final int ENC_UTF8 = 3;
 
+    /** Reusable UTF-8 validation object instance. */
     protected final UTF8 utf8 = new UTF8();
 
     /*
      * EOL.
      */
 
+    /** LF end of line identifier. */
     public static final int EOL_LF = 0;
+    /** CRLF end of line identifier. */
     public static final int EOL_CRLF = 1;
 
+    /** True if the previous character was a CR. */
     protected boolean bCr = false;
 
     /*
      * Configuration.
      */
 
+    /** Parses a headerline if true and a normal line if false. */
     public boolean bNameValue;
+    /** Identifier for the expected character encoding. */
     public int encoding = ENC_RAW;
+    /** Identifier for the Expected end of line character sequence. */
     public int eol = EOL_CRLF;
+    /** Support LWS. */
     public boolean bLWS;
+    /** Support quoted text. */
     public boolean bQuotedText;
+    /** Support encoded words. */
     public boolean bEncodedWords;
 
+    /** Reusable <code>StringBuffer</code> for lines. */
     protected final StringBuffer lineSb = new StringBuffer();
+    /** Reusable <code>StringBuffer</code> for name/value strings. */
     protected final StringBuffer nvSb = new StringBuffer();
+    /** Stream used to record the raw characters read by the parser. */
     protected UnreadableByteArrayOutputStream bytesOut = new UnreadableByteArrayOutputStream();
 
     /** Used by decode method to indicated valid or non valid character. */
@@ -111,15 +148,25 @@ public class HeaderLineReader {
      * Error reporting.
      */
 
+    /** Bit denoting unexpected EOF. */
     public static final int E_BIT_EOF = 2^0;
+    /** Bit denoting a misplaced CR. */
     public static final int E_BIT_MISPLACED_CR = 2^1;
+    /** Bit denoting a missing CR. */
     public static final int E_BIT_MISSING_CR = 2^2;
+    /** Bit denoting an excessive CR. */
     public static final int E_BIT_EXCESSIVE_CR = 2^3;
+    /** Bit denoting an invalid UTF-8 encoded character. */
     public static final int E_BIT_INVALID_UTF8_ENCODING = 2^4;
+    /** Bit denoting an invalud US-ASCII character. */
     public static final int E_BIT_INVALID_US_ASCII_CHAR = 2^5;
+    /** Bit denoting an invalid control character. */
     public static final int E_BIT_INVALID_CONTROL_CHAR = 2^6;
+    /** Bit denoting an invalid separator character. */
     public static final int E_BIT_INVALID_SEPARATOR_CHAR = 2^7;
+    /** Bit denoting a missing quote character. */
     public static final int E_BIT_MISSING_QUOTE = 2^8;
+    /** Bit denoting a missing quoted pair character. */
     public static final int E_BIT_MISSING_QUOTED_PAIR_CHAR = 2^9;
 
     /** Bit field of errors encountered while attempting to read a line. */
@@ -131,10 +178,19 @@ public class HeaderLineReader {
     protected HeaderLineReader() {
     }
 
+    /**
+     * Returns a reader with default configuration values.
+     * @return a reader with default configuration values
+     */
     public static HeaderLineReader getReader() {
         return new HeaderLineReader();
     }
 
+    /**
+     * Returns a reader to read normal lines. 
+     * The reader is pre-configured to expect US-ASCII characters.
+     * @return a reader to read normal lines
+     */
     public static HeaderLineReader getLineReader() {
         HeaderLineReader hlr = new HeaderLineReader();
         hlr.bNameValue = false;
@@ -142,6 +198,12 @@ public class HeaderLineReader {
         return hlr;
     }
 
+    /**
+     * Returns a reader to read header lines.
+     * The reader is pre- configured to expect UTF-8 encoding, LWS,
+     * quoted text and encoded words.
+     * @return a reader to read header lines
+     */
     public static HeaderLineReader getHeaderLineReader() {
         HeaderLineReader hlr = new HeaderLineReader();
         hlr.bNameValue = true;
@@ -152,6 +214,14 @@ public class HeaderLineReader {
         return hlr;
     }
 
+    /**
+     * Reads a header/line according to the configuration.
+     * After calling the readLine method additional information is available
+     * from public fields on the reader.
+     * @param in <code>InputStream</code> with characters
+     * @return result wrapped in a <code>HeaderLine</code> object
+     * @throws IOException
+     */
     public HeaderLine readLine(PushbackInputStream in) throws IOException {
         HeaderLine headerLine = new HeaderLine();
         int state;
@@ -525,6 +595,13 @@ public class HeaderLineReader {
         return headerLine;
     }
 
+    /**
+     * Decode a character according to the expected encoding.
+     * @param c first character of the possibly encoded character sequence
+     * @param in <code>InputStream</code> with possible extra encoded characters.
+     * @return decoded character
+     * @throws IOException
+     */
     protected int decode(int c, InputStream in) throws IOException {
         switch (encoding) {
         case ENC_UTF8:
@@ -554,6 +631,9 @@ public class HeaderLineReader {
         return c;
     }
 
+    /**
+     * Check and report whether a line was ended a expected.
+     */
     protected void check_eol() {
         switch (eol) {
         case EOL_LF:
@@ -572,6 +652,13 @@ public class HeaderLineReader {
         bCr = false;
     }
 
+    /**
+     * Trims the whitespace characters found in the beginning and end of a
+     * string. Differs from the String method in that it leaves control
+     * characters.
+     * @param sb <code>StringBuffer</code> to be trimmed
+     * @return trimmed string
+     */
     public static String trim(StringBuffer sb) {
         int sIdx = 0;
         int eIdx = sb.length();
