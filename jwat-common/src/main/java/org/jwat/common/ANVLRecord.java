26cc18685bf54e8867b6864a27b4c4916d5f6b11
Nicholas Clarke <nicl@kb.dk>
2015-03-23 23:46:16 +0100

    Added initial ANVLRecord class.

---
+++ jwat-common/src/main/java/org/jwat/common/ANVLRecord.java
@@ -0,0 +1,77 @@
+/**
+ * Java Web Archive Toolkit - Software to read and validate ARC, WARC
+ * and GZip files. (http://jwat.org/)
+ * Copyright 2011-2012 Netarkivet.dk (http://netarkivet.dk/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jwat.common;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Internet-Draft: draft-kunze-anvl-02.txt
+ * http://tools.ietf.org/id/draft-kunze-anvl-02.txt
+ *
+ * @author nicl
+ */
+public class ANVLRecord {
+
+	public static final String CRLF = "\r\n";
+
+	protected List<String> list = new LinkedList<String>();
+
+	protected Map<String, String> map = new HashMap<String, String>();
+
+	public ANVLRecord() {
+	}
+
+	public void add(String name, String value) {
+		if (value == null) {
+			throw new IllegalArgumentException("Invalid value!");
+		}
+		if (name == null || name.length() == 0) {
+			throw new IllegalArgumentException("Invalid name!");
+		}
+		list.add(name + ":" + value);
+		map.put(name, value);
+	}
+
+	public void add(String value) {
+		if (value == null || value.length() == 0) {
+			throw new IllegalArgumentException("Invalid value!");
+		}
+		list.add(value);
+	}
+
+	public byte[] getBytes(String charsetName) throws UnsupportedEncodingException {
+		StringBuilder sb = new StringBuilder();
+		Iterator<String> iter = list.iterator();
+		while (iter.hasNext()) {
+			sb.append(iter.next());
+			sb.append(CRLF);
+		}
+		sb.append(CRLF);
+		return sb.toString().getBytes(charsetName);
+	}
+
+	public ANVLRecord parse() {
+		return null;
+	}
+
+}
---
+++ jwat-common/src/test/java/org/jwat/common/TestANVLRecord.java
@@ -0,0 +1,86 @@
+/**
+ * Java Web Archive Toolkit - Software to read and validate ARC, WARC
+ * and GZip files. (http://jwat.org/)
+ * Copyright 2011-2012 Netarkivet.dk (http://netarkivet.dk/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jwat.common;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestANVLRecord {
+
+	@Test
+	public void test_anvlrecord() {
+		Object[][] cases = null;
+		try {
+			cases = new Object[][] {
+					{
+						new String[][] {
+						},
+						"\r\n".getBytes("UTF-8")
+					},
+					{
+						new String[][] {
+								{"entry", ""},
+								{null, "# first draft"},
+								{"who", "   Gilbert, W.S. | Sullivan, Arthur"},
+								{"what", "  The Yeomen of\r\n       the Guard"},
+								{"when/created", "  1888"}
+						},
+						("entry:\r\n"
+						+ "# first draft\r\n"
+						+ "who:   Gilbert, W.S. | Sullivan, Arthur\r\n"
+						+ "what:  The Yeomen of\r\n"
+						+ "       the Guard\r\n"
+						+ "when/created:  1888\r\n"
+						+ "\r\n").getBytes("UTF-8")
+					}
+			};
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			Assert.fail("Unexpected exception!");
+		}
+		try {
+			String name;
+			String value;
+			for (int i=0; i<cases.length; ++i) {
+				String[][] nvs = (String[][])cases[i][0];
+				byte[] expected = (byte[])cases[i][1];
+				ANVLRecord anvlRecord = new ANVLRecord();
+				for (int j=0; j<nvs.length; ++j) {
+					name = nvs[j][0];
+					value = nvs[j][1];
+					if (name != null) {
+						anvlRecord.add(name, value);
+					} else {
+						anvlRecord.add(value);
+					}
+				}
+				byte[] bytes = anvlRecord.getBytes("UTF-8");
+				Assert.assertArrayEquals(expected, bytes);
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			Assert.fail("Unexpected exception!");
+		}
+	}
+
+}
